936f633ac3c78cf695f41a6cd1f62fed44bdff2f
albdomrui <albdomrui@alum.us.es>
2022-11-03 17:16:45 +0100

    Examen

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomRepository.java
@@ -3,10 +3,20 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.util.List;
 import java.util.Optional;
 
-public interface RecoveryRoomRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecoveryRoomRepository extends CrudRepository<RecoveryRoom, Integer> {
     List<RecoveryRoom> findAll();
     Optional<RecoveryRoom> findById(int id);
+    
     RecoveryRoom save(RecoveryRoom p);
-    //List<RecoveryRoomType> findAllRecoveryRoomTypes();
-    //RecoveryRoomType getRecoveryRoomType(String name);
+    
+    @Query("SELECT p FROM RecoveryRoomType p")
+    List<RecoveryRoomType> findAllRecoveryRoomTypes();
+    @Query("SELECT p FROM RecoveryRoomType p WHERE p.name = ?1")
+    RecoveryRoomType getRecoveryRoomType(String name);
+    
+    
+
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
@@ -3,20 +3,32 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RecoveryRoomTypeFormatter implements Formatter<RecoveryRoomType>{
+	
+	private RecoveryRoomService rrs;
+	
+	@Autowired
+	public RecoveryRoomTypeFormatter(RecoveryRoomService rrs) {
+		this.rrs=rrs;
+	}
 
     @Override
     public String print(RecoveryRoomType object, Locale locale) {
-        return null;
+        return object.getName();
     }
 
     @Override
     public RecoveryRoomType parse(String text, Locale locale) throws ParseException {
-        return null;
+    	RecoveryRoomType rType = rrs.getRecoveryRoomType(text);
+      if (rType == null) {
+          throw new ParseException("RecoveryRoomType type not found: " + text, 0);
+      }
+      return rType;
     }
     
 }
